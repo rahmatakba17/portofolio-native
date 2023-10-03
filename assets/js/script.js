1c3987c1fd2428999aa7909aaa1b91c3b935b5bb
rahmatakba17 <rahmat_17@mhs.akba.ac.id>
2023-10-03 21:54:51 +0800

    perubahan warna tema dan sedikit perubahan animasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,12 @@ function myFunction(){
   var element = document.body;
   element.classList.toggle('dark-mode');
 }
+
+// window.onscroll = () => {
+//   let header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 100);
+// }
+document.addEventListener('scroll', () => {
+   const header= document.querySelector('.header');
+   header.classList.toggle('.sticky');
+})
